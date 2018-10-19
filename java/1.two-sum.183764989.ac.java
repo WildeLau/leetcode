8c355723f50a7aac54d41ac4e41bfe6b9eac1ad3
WildeLau <aihaofv@sina.com>
2018-10-19 09:45:11 +0800

    feat: add py3 and java solution of 1

---
+++ java/1.two-sum.183764989.ac.java
@@ -0,0 +1,46 @@
+/*
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (38.92%)
+ * Total Accepted:    1.1M
+ * Total Submissions: 2.9M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * Example:
+ * 
+ * 
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * 
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * 
+ * 
+ * 
+ * 
+ */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+                break;    
+            }
+            map.put(nums[i], i);
+        }
+
+        return result;
+    }
+}
+
